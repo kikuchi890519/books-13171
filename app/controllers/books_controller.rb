ad181b7dd2fe6a412daff86f34f8b18b099dd1d4
kikuchi890519 <kikuchi.890519@gmail.com>
2021-05-10 16:31:36 +0900

    編集機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:indew, :new, :create, :edit, :update]
+  before_action :set_book, only: [:edit, :show, :update]
  
   def index
     @books = Book.order('created_at DESC')
@@ -19,11 +20,27 @@ before_action :authenticate_user!, only: [:new]
   end
 
   def show
-    @book = Book.find(params[:id])
+  end
+
+  def edit
+    redirect_to root_path unless @book.user_id == current_user.id
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :author, :publishing_company_id, :category_id, :summary, :output, :image).merge(user_id: current_user.id)
   end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
 end
