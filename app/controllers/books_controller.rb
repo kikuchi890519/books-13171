99db830027fb8edd1b2a8bfbd6c8d930c6ee58b6
kikuchi890519 <kikuchi.890519@gmail.com>
2021-05-10 16:47:28 +0900

    削除機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!, only: [:indew, :new, :create, :edit, :update]
-  before_action :set_book, only: [:edit, :show, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_book, only: [:edit, :show, :update, :destroy]
+  before_action :redirect_root, only: [:edit, :update, :destroy]
  
   def index
     @books = Book.order('created_at DESC')
@@ -23,20 +24,27 @@ class BooksController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless @book.user_id == current_user.id
   end
 
   def update
     if @book.update(book_params)
-      redirect_to root_path
     else
       render :edit
     end
   end
 
+  def destroy
+    @book.destroy
+    redirect_to root_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :author, :publishing_company_id, :category_id, :summary, :output, :image).merge(user_id: current_user.id)
+  end
+
+  def redirect_root
+    redirect_to root_path unless @book.user_id = current_user.id
   end
 
   def set_book
